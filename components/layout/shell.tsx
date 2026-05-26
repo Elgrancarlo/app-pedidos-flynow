d161bbf1bc8e2a871c4186a640891aa059d68ee0
italo <italo.devv@gmail.com>
2026-05-26 12:39:55 -0300

    style: normalize dashboard cursors

--- components/layout/shell.tsx
+++ components/layout/shell.tsx
@@ -2,7 +2,7 @@ import Sidebar from "./sidebar";
 
 export default function Shell({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-dvh bg-[#050505] text-[#F5F2EA] [--sidebar-width:16rem]">
+    <div className="flynow-dashboard-shell min-h-dvh bg-[#050505] text-[#F5F2EA] [--sidebar-width:16rem]">
       <Sidebar />
       <main className="min-h-dvh bg-[#050505] pl-[var(--sidebar-width)]">
         {children}
